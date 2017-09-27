f0b2e3b3076b194be3515246b3f7951ca28bd6db
duxuanrain <598991476qq.com>
2017-09-27 15:03:21 +0800

    first commit

---
+++ apple.js
@@ -0,0 +1,26 @@
+$('.slides').on('click', '.controls li', function(e){
+    let $li = $(e.currentTarget);
+    let index = $li.index();
+    go(index);
+});
+
+let current = 0;
+
+setInterval(function(){
+    let nextIndex = current+1;
+    if(nextIndex === 4){
+        nextIndex = 0;
+    }
+
+    go(nextIndex);
+    current = nextIndex;
+},3000);
+
+function go(index){
+    $('.slides .controls li').eq(index).addClass('active')
+        .siblings().removeClass('active');
+    let width = $('.slides>.window').width();
+    $('.slides > .window > ol').css({
+        transform: 'translateX(' + (-width*index) +'px)'
+    });
+};
